d6b35cdd61bead948cf6b8565ea1bdeb98710768
Aaftab Pinjari <pinjari.aaftab@gmail.com>
2021-04-06 11:51:07 +0530

    minor change

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
 
   return (
     <Container className="my-4">
-    <h1>Jobs By Github</h1>
+    <h1>Jobs By Github.</h1>
   {/* <JobsPagination page={page} setPage={setPage} hasNextPage={true} /> */}
       {loading && <h1> Loading...</h1>}
       {error && <h1>Error. Try Rereshing</h1>}
